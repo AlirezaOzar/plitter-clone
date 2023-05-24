cc5d0d8883189b20fd38f2497e9e672f3dc67ee9
Alireza <mohanadozarr@gmail.com>
2023-05-24 16:46:13 +0330

    model ui is done

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
-import Layout from '@/components/Layout'
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Layout from "@/components/Layout";
+// import Model from "@/components/Model";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <Layout>
-    <Component {...pageProps}/>
-  </Layout>
-  ) 
+    <>
+      {/* <Model actionLabel="Submit" isOpen title="Test model"/> */}
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  );
 }
