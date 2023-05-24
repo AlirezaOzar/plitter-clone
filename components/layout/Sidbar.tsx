6211ab9de69a7f624a9c00b9084c0d7017442ef6
Alireza <mohanadozarr@gmail.com>
2023-05-24 13:06:24 +0330

    side bar component is done

---
+++ components/layout/Sidbar.tsx
@@ -0,0 +1,47 @@
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
+import {BiLogOut} from "react-icons/bi";
+import SidebarTwitteButton from "./SidebarTwitteButton";
+
+const Sibar = () => {
+  const items = [
+    {
+      label: "Home",
+      href: "/",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notfications",
+      href: "/notfications",
+      icon: BsBellFill,
+    },
+    {
+      label: "Profile",
+      href: "/users/123",
+      icon: FaUser,
+    },
+  ];
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => (
+            <SidebarItem
+              key={item.href}
+              href={item.href}
+              label={item.label}
+              icon={item.icon}
+            />
+          ))}
+          <SidebarItem onClick={() => {}} icon={BiLogOut} label="Logout"/>
+          <SidebarTwitteButton/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sibar;
