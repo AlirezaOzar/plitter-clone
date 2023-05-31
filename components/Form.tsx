2a03aa2a19d8e9faf0b5bd660b4f1ee93d447947
Alireza <mohanadozarr@gmail.com>
2023-05-31 23:46:13 +0330

    comment reply is done

--- components/Form.tsx
+++ components/Form.tsx
@@ -8,6 +8,7 @@ import { toast } from "react-hot-toast";
 import axios from "axios";
 import Button from "./Button";
 import Avatar from "./Avatar";
+import usePost from "@/hooks/usePost";
 
 interface FormProps {
   placeholder: string;
@@ -20,51 +21,59 @@ const Form: React.FC<FormProps> = ({ placeholder, isComment, postId }) => {
   const loginModal = useLoginModel();
 
   const { data: currentUser } = useCurrentUser();
-  const { mutate: mutatePosts } = usePosts(postId as string);
-
+  const { mutate: mutatePosts } = usePosts();
+  const { mutate: mutatePost } = usePost(postId as string);
   const [body, setBody] = useState("");
   const [isLoading, setIsLoading] = useState(false);
-
   const onSubmit = useCallback(async () => {
     try {
       setIsLoading(true);
-      await axios.post("/api/posts", { body });
+
+      // we make it dynamic if this is a form for the global feed right here or if this is a form for a reply
+      const url = isComment ? `/api/comments?postId=${postId}` : `/api/posts`; // we
+
+      await axios.post(url, { body });
 
       toast.success("Tweet Created");
 
       setBody("");
       mutatePosts();
+      mutatePost();
     } catch (error) {
       toast.error("Something went wrong");
       console.log(error);
     } finally {
       setIsLoading(false);
     }
-  }, [body, mutatePosts]);
+  }, [body, mutatePosts, isComment, postId, mutatePost]);
   return (
     <div className="border-b-[1px] border-neutral-800 px-5 py-2">
       {currentUser ? (
         <div className="flex flex-row gap-4">
           <div>
-            <Avatar userId={currentUser?.id}/>
+            <Avatar userId={currentUser?.id} />
           </div>
           <div className="w-full">
             <textarea
-             disabled={isLoading}
-             onChange={(e) => setBody(e.target.value)}
-             value={body}
-             className=" 
+              disabled={isLoading}
+              onChange={(e) => setBody(e.target.value)}
+              value={body}
+              className=" 
              disabled:opacity-80 peer 
              resize-none mt-3 w-full 
              bg-black ring-0 outline-none
               text-[20px] placeholder-neutral-500
                text-white
              "
-             placeholder={placeholder}
+              placeholder={placeholder}
             ></textarea>
-            <hr  className="opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition"/>
+            <hr className="opacity-0 peer-focus:opacity-100 h-[1px] w-full border-neutral-800 transition" />
             <div className="mt-4 flex flex-row justify-end">
-               <Button label="Tweet" disabled={isLoading || !body} onClick={onSubmit}/>
+              <Button
+                label="Tweet"
+                disabled={isLoading || !body}
+                onClick={onSubmit}
+              />
             </div>
           </div>
         </div>
